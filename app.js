330c4fe051fbaf75b06936baef954488cf5aec02
Maru Karthik Reddy <karthikreddy.maru@gmail.com>
2025-12-13 22:01:08 +0530

    feat(db): add mongo driver

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express')
 
 const routes = require('./routes/ProductRoutes')
-const sequelize = require('./repository/database');
 const app = express()
 const PORT = 9090
 
 app.use(routes)
 
 app.listen(PORT, () => {
-    sequelize.authenticate().then(result => {
-        console.log("Database connected");
-        console.log(`Server started at ${PORT}`);
-    }).catch(err => console.log(err));
+    console.log(`Server started at ${PORT}`);
 })
--- repository/database.js
+++
@@ -1,7 +0,0 @@
-const {Sequelize} = require('sequelize')
-
-module.exports = new Sequelize('ecom_nodejs', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-})
